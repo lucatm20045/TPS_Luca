0b26d0fb6373101ad87c787376a5cd466fdbcd51
lucarolla12 <lucarolla08@gmail.com>
2026-02-17 15:36:36 +0100

    Create es03b.js

---
+++ es03b.js
@@ -0,0 +1,13 @@
+let m= parseFloat(prompt("Inserisci m:"));
+let c = parseFloat(prompt("Inserisci c:"));
+let x1 = - 3 ;
+let x2 = 5 ;
+let y1 = m * x1 + с;
+let y2 =m * x2 + c;
+console.log("Per x =", x1, ", y", y1);
+console.log("Per x =", x2, ", y", y2);
+// Intersezioni
+let intersezioney = {x: 0. y: c};
+let intersezionex = {x: c/m, y: 0};
+console.log("Intersezione con l'asse Y:", intersezioneY);
+console.log("Intersezione con l'asse X:", intersezionex);
