dca9a129da1b2faef6211e2dfb9cc56c0567a664
lucarolla12 <lucarolla08@gmail.com>
2026-02-17 15:31:18 +0100

    Create Es03A.js

---
+++ Es03A.js
@@ -0,0 +1,5 @@
+let larghezza =parseFloat(prompt("inserisci la larghezza: "));
+let altezza =parseFloat(prompt(" inserisci l'altezza: "));
+let profondita parseFloat(prompt("Inserisci la profondità:"));
+let volume larghezza altezza profondita;
+console.log("Il volume della scatola è: " volume);
