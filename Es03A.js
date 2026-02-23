d416de3305bee4cbbe63f851fa5c5f9fa23cf933
lucarolla12 <lucarolla08@gmail.com>
2026-02-23 18:45:35 +0100

    Update es03a.js

--- Es03A.js
+++ Es03A.js
@@ -1,5 +1,11 @@
-let larghezza =parseFloat(prompt("inserisci la larghezza: "));
-let altezza =parseFloat(prompt(" inserisci l'altezza: "));
-let profondita parseFloat(prompt("Inserisci la profondità:"));
-let volume larghezza altezza profondita;
-console.log("Il volume della scatola è: " volume);
+let larghezza = prompt("Inserisci la larghezza della scatola:");
+let altezza = prompt("Inserisci l'altezza della scatola:");
+let lunghezza = prompt("Inserisci la lunghezza della scatola:");
+
+larghezza = Number(larghezza);
+altezza = Number(altezza);
+lunghezza = Number(lunghezza);
+
+let volume = larghezza * altezza * lunghezza;
+
+console.log("Il volume della scatola è: " + volume);
