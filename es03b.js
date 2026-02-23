6a5505d85c69e7ed8f7734e6d5adb4f931342019
lucarolla12 <lucarolla08@gmail.com>
2026-02-23 19:18:43 +0100

    Update es03b.js

--- es03b.js
+++ es03b.js
@@ -1,13 +1,15 @@
-let m= parseFloat(prompt("Inserisci m:"));
-let c = parseFloat(prompt("Inserisci c:"));
-let x1 = - 3 ;
-let x2 = 5 ;
-let y1 = m * x1 + с;
-let y2 =m * x2 + c;
-console.log("Per x =", x1, ", y", y1);
-console.log("Per x =", x2, ", y", y2);
-// Intersezioni
-let intersezioney = {x: 0. y: c};
-let intersezionex = {x: c/m, y: 0};
-console.log("Intersezione con l'asse Y:", intersezioneY);
-console.log("Intersezione con l'asse X:", intersezionex);
+let m = Number(prompt("Inserisci il coefficiente angolare m:"));
+let c = Number(prompt("Inserisci il termine noto c:"));
+
+let y1 = m * (-1) + c;
+let y2 = m * 10 + c;
+
+let intersezioneY = c;
+
+let intersezioneX = -c / m;
+
+console.log("Per x = -1, y = " + y1);
+console.log("Per x = 10, y = " + y2);
+
+console.log("Intersezione con l'asse Y: (0, " + intersezioneY + ")");
+console.log("Intersezione con l'asse X: (" + intersezioneX + ", 0)");
